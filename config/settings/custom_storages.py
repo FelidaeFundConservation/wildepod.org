f518a2615c7a70df2b1ac11e42c437cfcc7ed6f8
Abhay Kashyap <abhaykash666@gmail.com>
2022-01-05 06:00:44 -0800

    Added custom storage file

---
+++ config/settings/custom_storages.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from storages.backends.gcloud import GoogleCloudStorage
+
+class StaticStorage(GoogleCloudStorage):
+    bucket_name = settings.GS_STATIC_STORAGE_BUCKET_NAME
+
+class MediaStorage(GoogleCloudStorage):
+    bucket_name = settings.GS_MEDIA_STORAGE_BUCKET_NAME

01f13b4344ee259600f2d73735cfc52d2eb6cef3
Abhay Kashyap <abhaykash666@gmail.com>
2022-02-24 01:40:41 -0800

    Finished email integration and species annotation. Site is now functionally complete

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -74,14 +74,14 @@ MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/media/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="admin <noreply@wildepod.org>",
+    default="Admin <noreply@wildepod.org>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[WildePod]",
+    default="[WildePod.org]",
 )
 # Sendgrid email settings
 SENDGRID_API_KEY = env("SENDGRID_API_KEY")

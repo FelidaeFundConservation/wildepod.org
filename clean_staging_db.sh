d53fc0949bc5b8b5a11ece21f0ebeeb7a6a47d5d
Abhay Kashyap <abhaykash666@gmail.com>
2022-02-27 15:54:36 -0800

    Updated helper script

--- clean_staging_db.sh
+++ clean_staging_db.sh
@@ -2,9 +2,5 @@
 find -name "000*.py" -delete
 python manage.py makemigrations --settings=config.settings.staging
 python manage.py migrate --settings=config.settings.staging
-export DJANGO_SUPERUSER_USERNAME="test"
-export DJANGO_SUPERUSER_PASSWORD="test1234"
-python manage.py createsuperuser --noinput --email test@test.com --settings=config.settings.staging
 python manage.py shell --settings=config.settings.staging < scratch/load.py
 python manage.py collectstatic --noinput --clear --settings=config.settings.staging
-python manage.py runserver --settings=config.settings.staging

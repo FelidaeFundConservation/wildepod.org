34e15a2e95e204b71b0bc75a184d7e56e2a4e730
Abhay Kashyap <abhaykash666@gmail.com>
2022-03-30 21:51:28 -0700

    Skip processed images

--- config/settings/base.py
+++ config/settings/base.py
@@ -232,26 +232,6 @@ SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "DENY"
 
 
-# LOGGING
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#logging
-# See https://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {"verbose": {"format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"}},
-    "handlers": {
-        "console": {
-            "level": "DEBUG",
-            "class": "logging.StreamHandler",
-            "formatter": "verbose",
-        }
-    },
-    "root": {"level": "INFO", "handlers": ["console"]},
-}
-
-
 # JAZZMIN
 # ------------------------------------------------------------------------------
 # Jazzmin admin site customization

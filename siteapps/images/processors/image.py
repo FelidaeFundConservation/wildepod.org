ac148bed559c882303bc3ab2f674d3885ebfad8b
Abhay Kashyap <abhaykash666@gmail.com>
2022-04-02 22:54:20 -0700

    Peppered code with logging statements

--- siteapps/images/processors/image.py
+++ siteapps/images/processors/image.py
@@ -39,6 +39,7 @@ def add_thumbnail(image: Image):
 
     Returns the full storage url
     """
+    logging.info("Retrieving thumbnail from dropbox..")
     # TODO: Handle error or Rollback on failure
     # Get a 1024x1024 thumbnail from dropbox.
     # This resolution should also work well for most ML models trained on top of resnet etc.
@@ -48,37 +49,53 @@ def add_thumbnail(image: Image):
         size=dropbox.files.ThumbnailSize("w1024h768", None),
         mode=dropbox.files.ThumbnailMode("bestfit", None),
     )
+    logging.info("Successfully retrieved thumbnail from dropbox.")
 
     # Convert image binary to bytestream
     img_bytes = BytesIO(response.content)
+    logging.info("Successfully converted thumbnail binary to bytestream.")
+
     target_path = f"compressed/1024/{image.dropbox_content_hash}.jpg"
     gcloud_path = storage.save(target_path, img_bytes)
+    logging.info("Successfully saved thumbnail to google cloud storage.")
 
     image.thumbnail_gcloud_path = gcloud_path
     # Save the image
     image.save()
-
+    logging.info("Successfully saved image object to database.")
 
 
 def add_bounding_boxes(image: Image):
     """Function to add bounding boxes to an image using MegaDetector's object detection model"""
     # TODO: Handle error or Rollback on failure
-    # Next, run MegaDetector on each image and create the relevant annotation objects
-    image_url = f"""gs://{settings.GS_BUCKET_NAME}/media/{image.thumbnail_gcloud_path}"""
+    # Run MegaDetector on each image and create the relevant annotation objects
     # TODO: Probably a better way to handle this. Hardcoded for now. Might not even need a model/record for this
-    bot, _ = Bot.objects.get_or_create(
+    bot, created = Bot.objects.get_or_create(
         name="MegaDetector",
         version="4.1.0",
         task_type="Object Detection",
         model_api_url=settings.MEGADETECTOR_URL,
         model_file_url=f"gs://{settings.MODEL_STORAGE_BUCKET}/md_v4.1.0.pb",
     )
-    annotator, _ = Annotator.objects.get_or_create(type="bot", bot=bot)
+    if created:
+        logging.info("Megadetector 4.1.0 objectect detection bot successfully created")
+    else:
+        logging.info("Megadetector 4.1.0 objectect detection bot already exists. Successfully retrieved.")
+
+    annotator, created = Annotator.objects.get_or_create(type="bot", bot=bot)
+    if created:
+        logging.info("Megadetector annotator object successfully created")
+    else:
+        logging.info("Megadetector annotator object already exists. Successfully retrieved.")
+
+    image_url = f"""gs://{settings.GS_BUCKET_NAME}/media/{image.thumbnail_gcloud_path}"""
+    logging.info(f"Calling MegaDetector on image with url - {image_url}")
     # Call the MegaDetector cloud function
     # There is a really high timeout here since the cloud function takes a while to start on first request
     response = http.post(bot.model_api_url, json={"image": image_url, "model": bot.model_file_url}, timeout=300)
     if response.status_code == 200:
         result = response.json()
+        logging.info(f"""MegaDetector cloud function call successful. {len(result["detections"])} objects detected""")
     else:
         logging.error(f"MegaDetector cloud function failed with status code: {response.status_code}")
         return
@@ -86,7 +103,7 @@ def add_bounding_boxes(image: Image):
 
     # For each detected bounding box, create a corresponding annotation object
     # Confidence for bounding box & category are the same for MegaDetector
-    for detection in result["detections"]:
+    for i, detection in enumerate(result["detections"]):
         # Create a new annotation object
         bounding_box = BoundingBox.objects.create(
             image=image,
@@ -104,6 +121,11 @@ def add_bounding_boxes(image: Image):
             created_by=annotator,
             confidence=detection["conf"],
         )
+        logging.info(
+            f"""Successfully created bounding box for object #{i+1} - {detection["category"]} (confidence - {detection["conf"]})"""
+        )
+
+    logging.info("All bounding boxes created successfully.")
 
 
 # Function to process an image
@@ -111,12 +133,18 @@ def process_image(image: Image):
     """Function to process an image and create relevant metadata"""
     # First, add a thumbnail to the image object if it doesn't already exist
     if not image.thumbnail_gcloud_path:
+        logging.info("Thumbnail for image doesn't exist. Adding..")
         add_thumbnail(image)
+    else:
+        logging.info("Thumbnail for image already exists. Skipping..")
+
+    logging.info("Adding bounding boxes to image..")
     # Next, add bounding boxes to the image object
     add_bounding_boxes(image)
+    logging.info("Finished adding bounding boxes to image..")
 
     image.processed = True
     image.save()
+    logging.info("Successfully saved image to database.")
 
     # Additional Species detection annotations go here.
-

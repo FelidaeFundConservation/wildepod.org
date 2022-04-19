dbf028b3bf9dc9ab0522bbc967e8690a9fa03c77
Abhay Kashyap <abhaykash666@gmail.com>
2022-04-18 17:02:16 -0700

    Added hacky page to view annotation stats for testing

--- siteapps/explore/models.py
+++ siteapps/explore/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from inventory.models import Camera, Padlock, PythonLock
+from model_utils.models import TimeStampedModel
+
+
+# Model to track exports, creators & its link
+class Export(TimeStampedModel):
+    # UUID for the upload
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Volunteer/Staff that requested the data export
+    volunteer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    # Export time
+    datetime = models.DateTimeField("Export Creation Time")
+
+    def __str__(self):
+        return f"{self.datetime}-{self.id}"
+
+    class Meta:
+        ordering = ("-datetime",)

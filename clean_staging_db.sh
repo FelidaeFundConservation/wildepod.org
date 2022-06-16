c1ec3de645edbcc79cb3468a1bb239ffd60569f4
Abhay Kashyap <abhaykash12@gmail.com>
2022-06-16 14:31:22 -0700

    Cleaned up location models

--- clean_staging_db.sh
+++ clean_staging_db.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-find -name "000*.py" -delete
 python manage.py makemigrations --settings=config.settings.staging
 python manage.py migrate --settings=config.settings.staging
 python manage.py shell --settings=config.settings.staging < scratch/load.py

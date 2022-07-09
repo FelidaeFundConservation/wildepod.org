0994c0171c8a6c1d70cb08944a2d0bf30d653eb9
Abhay Kashyap <abhaykash12@gmail.com>
2022-07-09 16:30:08 -0700

    Moved from css to sass. Updated theme & landing pages

--- siteapps/explore/views/map.py
+++ siteapps/explore/views/map.py
@@ -1,11 +1,10 @@
-from braces.views import StaffuserRequiredMixin
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from locations.models import CameraStation
 
 
-class ExploreMapView(LoginRequiredMixin, StaffuserRequiredMixin, ListView):
+class ExploreMapView(LoginRequiredMixin, ListView):
     login_url = settings.LOGIN_URL
     model = CameraStation
     template_name = "explore/map.html"

a61aeef605ebd9b4c3c226fc7d8617970d50c25f
Abhay Kashyap <abhaykash12@gmail.com>
2022-08-07 22:06:20 -0700

    Minor UI changes

--- clean_local_db.sh
+++ clean_local_db.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations --settings=config.settings.local
 python manage.py migrate --settings=config.settings.local
 python manage.py shell --settings=config.settings.local < scratch/load.py
 export DJANGO_SUPERUSER_PASSWORD="test123"
-python manage.py createsuperuser --noinput --email abhay@abhay.com --name abhay --settings=config.settings.local
+python manage.py createsuperuser --noinput --email test@test.com --name test --settings=config.settings.local
 python manage.py runserver --settings=config.settings.local

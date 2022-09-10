3be4582a38b5a995ab31182a19911e69b94eab60
Abhay Kashyap <abhaykash12@gmail.com>
2022-09-09 19:02:28 -0700

    Updated Drobox baseurls

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -59,7 +59,7 @@ EMAIL_TIMEOUT = 5
 DROPBOX_APP_KEY = env("DROPBOX_APP_KEY_STAGING")
 DROPBOX_APP_SECRET = env("DROPBOX_APP_SECRET_STAGING")
 DROPBOX_REFRESH_TOKEN = env("DROPBOX_REFRESH_TOKEN_STAGING")
-DROPBOX_URL_PREFIX = "https://www.dropbox.com/home/Felidae%20Fund/Apps/WildePod_staging"
+DROPBOX_URL_PREFIX = "https://www.dropbox.com/work/WildePod%20Cloud%20DB/Apps/wildepod_prod"
 
 
 # LOGGING

aaeca6bce78a744363a62b258c52b08036dfb183
prabath <prabathg@gmail.com>
2023-01-27 00:10:08 -0800

    1) Adding a new service jobs to handle long running tasks 2) Adding a new siteapp exports and moving the export functionality to it

---
+++ config/settings/jobs.py
@@ -0,0 +1,149 @@
+"""This is the staging settings that links to a staging database and will be deployed as its own service in within app engine
+"""
+import google.cloud.logging
+
+from .base import *  # noqa
+
+# HOSTS CONFIG
+# ------------------------------------------------------------------------------
+# NOTE: SECURITY WARNING: App Engine's security features ensure that it is safe to
+# have ALLOWED_HOSTS = ['*'] when the app is deployed. If you deploy a Django
+# app not on App Engine, make sure to set an appropriate host here.
+ALLOWED_HOSTS = ["*"]
+
+# DEBUG MODE
+# ------------------------------------------------------------------------------
+DEBUG = False
+
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = "config.wsgi.staging.application"
+
+# DATABASES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {"default": env.db("STAGING_DATABASE_URL")}
+
+# # If the flag has been set, configure to use proxy
+if env.bool("USE_CLOUD_SQL_AUTH_PROXY", False):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5440
+# TODO: Check if this needs to be done
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+
+# ADMIN
+# ------------------------------------------------------------------------------
+# Admin url obfuscation
+ADMIN_URL_SUFFIX = env.str("ADMIN_URL_SUFFIX", default="")
+
+
+ 
+# SECURITY
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
+#SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
+SESSION_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
+CSRF_COOKIE_SECURE = True
+# https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
+# TODO: set this to 60 seconds first and then to 518400 once you prove the former works
+SECURE_HSTS_SECONDS = 60
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-include-subdomains
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS", default=True)
+# https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-preload
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+# https://docs.djangoproject.com/en/dev/ref/middleware/#x-content-type-options-nosniff
+SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", default=True)
+
+
+
+# GCP settings
+# ------------------------------------------------------------------------------
+GS_BUCKET_NAME = env("GS_BUCKET_NAME_STAGING")
+GS_DEFAULT_ACL = "publicRead"
+# Static files
+STATICFILES_STORAGE = "siteapps.utils.storages.StaticRootGoogleCloudStorage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.gcloud.GoogleCloudStrategy"
+STATIC_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/static/"
+COMPRESS_STORAGE = STATICFILES_STORAGE
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+
+# Media files
+DEFAULT_FILE_STORAGE = "siteapps.utils.storages.MediaRootGoogleCloudStorage"
+MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/media/"
+
+CKEDITOR_UPLOAD_PATH = "ckeditor_uploads/"
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
+DEFAULT_FROM_EMAIL = env(
+    "DJANGO_DEFAULT_FROM_EMAIL",
+    default="admin <noreply@wildepod.org>",
+)
+# https://docs.djangoproject.com/en/dev/ref/settings/#server-email
+SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
+EMAIL_SUBJECT_PREFIX = env(
+    "DJANGO_EMAIL_SUBJECT_PREFIX",
+    default="[WildePod]",
+)
+# Sendgrid email settings
+SENDGRID_API_KEY = env("SENDGRID_API_KEY")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+# LOGGING
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# https://googleapis.dev/python/logging/latest/std-lib-integration.html
+# Logging into Google Cloud
+
+# Instantiates a client
+google_logging_client = google.cloud.logging.Client(project=env("GOOGLE_CLOUD_PROJECT"))
+
+# Retrieves a Cloud Logging handler based on the environment
+# you're running in and integrates the handler with the
+# Python logging module. By default this captures all logs
+# at INFO level and higher
+google_logging_client.setup_logging()
+
+
+# COMPRESSOR
+# ------------------------------------------------------------------------------
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+
+COMPRESS_PRECOMPILERS = (
+    ("text/x-sass", "sass --style compressed {infile} {outfile}"),
+    ("text/x-scss", "sass --style compressed {infile} {outfile}"),
+)
+
+
+# EXTERNAL APPS CONFIG
+# ------------------------------------------------------------------------------
+# Dropbox token for local mode
+DROPBOX_APP_KEY = env("DROPBOX_APP_KEY_STAGING")
+DROPBOX_APP_SECRET = env("DROPBOX_APP_SECRET_STAGING")
+DROPBOX_REFRESH_TOKEN = env("DROPBOX_REFRESH_TOKEN_STAGING")
+DROPBOX_URL_PREFIX = "https://www.dropbox.com/work/WildePod%20Cloud%20DB/Apps/wildepod_staging"
+
+# CUSTOM VARIABLES
+# ------------------------------------------------------------------------------
+# Annotation configuration
+NUM_ACCEPTS_OVER_REJECTS = 0
+
+# FLAGS DEFINED ONLY FOR EXPORT SERVICE 
+INSTALLED_APPS.append("siteapps.exports")
+EXPORT_SERVICE = True
+DEBUG = True
--- siteapps/explore/views/snapshot.py
+++ siteapps/explore/views/snapshot.py
@@ -1,236 +1,20 @@
-import csv
-from itertools import groupby
+import json
 import logging
-import threading
-import zipfile
-from io import BytesIO, StringIO
 
 from braces.views import StaffuserRequiredMixin
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.base import File as DjangoFile
+from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.urls.base import reverse_lazy
-from django.views.generic import FormView, ListView
+from django.views.generic import FormView, ListView, View
 from explore.forms import CreateSnapshotForm
 from explore.models import Snapshot
-from images.models import Image
-from locations.models import CameraStation
+from google.cloud import tasks_v2
 
 MAX_VOTES_PER_IMAGE = 2
-
-
-def export_camera_station_data(archive_file):
-    """Hacky function to export camera station data to a csv file"""
-    with StringIO() as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter="\t", quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(
-            [
-                "camera_station_id",
-                "latitude",
-                "longitude",
-                "micro_site",
-                "macro_site",
-                "county",
-                "area",
-                "elevation",
-                "elevation_units",
-                "habitat_type",
-                "habitat_notes",
-                "land_use_type",
-                "trail_type",
-                "trail_surface",
-                "felidae_camera",
-                "external_camera",
-                "date_deployed",
-                "date_last_checked",
-                "date_to_be_checked",
-                "date_taken_down",
-                "boxed",
-                "padlock_type",
-                "python_lock",
-                "linked_volunteers",
-                "comments",
-            ]
-        )
-
-        # Get all camera stations
-        camera_stations = CameraStation.objects.all()
-
-        for camera_station in camera_stations:
-            csv_writer.writerow(
-                [
-                    camera_station.station_id,
-                    camera_station.latitude,
-                    camera_station.longitude,
-                    camera_station.micro_site,
-                    camera_station.micro_site.macro_site,
-                    camera_station.micro_site.macro_site.county,
-                    camera_station.micro_site.macro_site.county.area,
-                    camera_station.elevation,
-                    camera_station.elevation_unit,
-                    " | ".join([habitat_type.name for habitat_type in camera_station.habitat_types.all()]),
-                    camera_station.habitat_notes,
-                    " | ".join([land_use_type.name for land_use_type in camera_station.land_use_type.all()]),
-                    camera_station.trail_type,
-                    camera_station.trail_surface,
-                    camera_station.camera,
-                    camera_station.external_camera,
-                    camera_station.date_deployed,
-                    camera_station.date_last_checked,
-                    camera_station.date_to_be_checked,
-                    camera_station.date_taken_down,
-                    camera_station.boxed,
-                    camera_station.padlock,
-                    camera_station.python_lock,
-                    " | ".join([volunteer.name for volunteer in camera_station.volunteer.all()]),
-                    camera_station.comments,
-                ]
-            )
-        logging.info("Finished creating a csv file for camera stations")
-        archive_file.writestr("camera_stations.tsv", csv_file.getvalue())
-        logging.info("Finished writing camera station csv to archive")
-
-
-def export_image_data(archive_file, images):
-    """Hacky function to export camera station data to a csv file"""
-    with StringIO() as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter="\t", quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow(
-            [
-                "image_id",
-                "content_hash",
-                "filename",
-                "thumbnail",
-                "dropbox_link",
-                "trigger_timestamp",
-                "exif_latitude",
-                "exif_longitude",
-                "is_video",
-                "camera_station_id",
-                "micro_site",
-                "macro_site",
-                "date_retrieved",
-                "volunteer",
-                "upload_folder",
-                "social_media_worthy_vote_count",
-                "blank_checked_by",
-                "detected_objects",
-                "validated_objects",
-                "uncertain_objects",
-                "objects",
-                "species_checked_by",
-                "validated_species",
-                "uncertain_species",
-                "species",
-            ]
-        )
-
-        for i, image in enumerate(images):
-            valid_bounding_boxes = image.boundingbox_set.valid()
-            uncertain_bounding_boxes = image.boundingbox_set.uncertain()
-            valid_or_uncertain_bounding_boxes = image.boundingbox_set.valid_or_uncertain()
-
-            # We need to separate out the categories and the species when there are multiple annotations for the same image
-            # If there are multiple catrgories/species, we will generate multiple rows in the output per each group.
-            if valid_bounding_boxes:
-                # Use groupby and a lambda function to split the bounding boxes into subgroups based on the category name
-                category_groups = groupby(valid_bounding_boxes, lambda x: x.category_set.first().name)
-                for category_name, bbox_categories in category_groups:
-                    # Use groupby and a lambda function to split the bounding boxes into subgroups based on the species name
-                    species_groups = groupby(bbox_categories,
-                                    lambda x: x.species_set.first().name.name if x.species_set.first() else '')
-                    for species_name, bbox_species in species_groups:
-                        write_row(csv_writer, image, category_name, species_name, list(bbox_species), uncertain_bounding_boxes, valid_or_uncertain_bounding_boxes)
-            else:
-                write_row(csv_writer, image, '', '', valid_bounding_boxes, uncertain_bounding_boxes, valid_or_uncertain_bounding_boxes)
-
-            if i % 100 == 0:
-                logging.info(f"Finished {i}/{len(images)} images")
-
-        logging.info("Finished creating a csv file for images")
-        archive_file.writestr("images.tsv", csv_file.getvalue())
-        logging.info("Finished writing image csv to archive")
-
-def write_row(csv_writer, image, category_name, species_name, valid_bounding_boxes, uncertain_bounding_boxes, valid_or_uncertain_bounding_boxes):
-    output_list = [
-                    image.id,
-                    image.dropbox_content_hash,
-                    image.dropbox_file_name,
-                    f"""https://storage.googleapis.com/{settings.GS_BUCKET_NAME}/media/{image.thumbnail_gcloud_path}""",
-                    f"""{settings.DROPBOX_URL_PREFIX}/{image.dropbox_file_path}""",
-                    image.trigger_timestamp,
-                    image.latitude,
-                    image.longitude,
-                    image.is_video,
-                    image.upload.camera_station.station_id,
-                    image.upload.camera_station.micro_site,
-                    image.upload.camera_station.micro_site.macro_site,
-                    image.upload.date_retrieved,
-                    image.upload.volunteer,
-                    f"""{settings.DROPBOX_URL_PREFIX}/{image.upload.dropbox_folder_path}""",
-                    image.social_media_worthy,
-                    len(image.bbox_checked_by.all()),
-                    len(valid_or_uncertain_bounding_boxes),
-                    len(valid_bounding_boxes),
-                    len(uncertain_bounding_boxes),
-                    category_name,
-                    len(image.species_checked_by.all()),
-                    len([True for bbox in valid_bounding_boxes if bbox.species_set.valid().exists()]),
-                    len([True for bbox in valid_bounding_boxes if bbox.species_set.uncertain().exists()]),
-                    species_name,
-                ]
-    csv_writer.writerow(output_list)
-
-
-def create_snapshot(cleaned_form_data, user):
-    """This is a hacky function to create a snapshot inside a thread and update the object when done"""
-    # First, use the form data to retrieve a filtered set of images
-    filterset = {}
-    start_date = cleaned_form_data["start_date"]
-    if start_date:
-        filterset["trigger_timestamp__gte"] = start_date
-    end_date = cleaned_form_data["end_date"]
-    if end_date:
-        filterset["trigger_timestamp__lte"] = end_date
-    macrosites = cleaned_form_data["macrosites"]
-    if macrosites:
-        filterset["upload__camera_station__micro_site__macro_site__in"] = macrosites
-    # annotated_only = cleaned_form_data["annotated_only"]
-
-    images = Image.objects.annotated().filter(**filterset)
-
-    # Next, create a snapshot object
-    snapshot = Snapshot.objects.create(
-        volunteer=user,
-        start_date=start_date,
-        end_date=end_date,
-        # annotated_only=annotated_only,
-    )
-    snapshot.macrosites.set(macrosites)
-
-    try:
-        # Create an archive file to house all the csvs
-        zipped_file = BytesIO()
-        with zipfile.ZipFile(zipped_file, "w") as archive_file:
-
-            # Export a multitude of csvs
-            # Start with camera stations themselves
-            export_camera_station_data(archive_file)
-
-            # Then export image data
-            export_image_data(archive_file, images)
-
-            # Create a Django file object for this
-            archive_obj = DjangoFile(zipped_file, name=f"{snapshot}.zip")
-
-        logging.info("Finished creating archive file")
-        snapshot.data = archive_obj
-        snapshot.status = "done"
-    except Exception as e:
-        snapshot.status = "failed"
-        logging.error(f"Error while creating snapshot: {e}")
-    snapshot.save()
 
 
 class SnapshotCreateView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
@@ -243,13 +27,59 @@ class SnapshotCreateView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
         form = CreateSnapshotForm(request.POST)
 
         if form.is_valid():
-            logging.info(f"Starting a thread to create a snapshot with filters - {form.cleaned_data}")
-            # Create a thread to process the upload
-            thread = threading.Thread(target=create_snapshot, args=[form.cleaned_data, self.request.user])
-            # Move it to the background
-            thread.setDaemon(True)
-            # Start running the thread
-            thread.start()
+            # Create a client.
+            client = tasks_v2.CloudTasksClient()
+
+            # TODO(developer): Uncomment these lines and replace with your values.
+            project = settings.GCP_PROJECT_ID
+            queue = settings.EXPORT_QUEUE_NAME
+            location = settings.GCP_REGION
+
+            # Construct the fully qualified queue name.
+            parent = client.queue_path(project, location, queue)
+
+            payload = {}
+            payload["user"] = self.request.user.pk
+            start_date = form.cleaned_data["start_date"]
+            if start_date:
+                print(f"Start date {start_date.isoformat()}")
+                payload["start_date"] = start_date.strftime(settings.EXPORT_DATE_FORMAT)
+            end_date = form.cleaned_data["end_date"]
+            if end_date:
+                payload["end_date"] = end_date.strftime(settings.EXPORT_DATE_FORMAT)
+            macrosites = form.cleaned_data["macrosites"]
+            if macrosites:
+                payload["macrosites"] = []
+                for site in macrosites:
+                    print(site.pk)
+                    payload["macrosites"].append(site.pk)
+
+            payload_json = json.dumps(payload, cls=DjangoJSONEncoder)
+
+            # The API expects a payload of type bytes.
+            converted_payload = payload_json.encode()
+
+            # Construct the request body.
+            task = {
+                "app_engine_http_request": {  # Specify the type of request.
+                    "http_method": tasks_v2.HttpMethod.POST,
+                    "relative_uri": f"/exports/start/{settings.EXPORT_URL_SUFFIX}/",
+                    "app_engine_routing": {
+                        "service": settings.EXPORT_SERVICE_NAME,
+                    },
+                    "headers": {
+                        #'X-CSRFToken': csrf_token, # This doesn't stop the CSRF error.
+                        "Content-Type": "application/json",
+                    },
+                    "body": converted_payload,
+                }
+            }
+
+            logging.info(f"Task details {task}")
+
+            # Use the client to build and send the task.
+            response = client.create_task(parent=parent, task=task)
+            logging.info("Created task {}".format(response.name))
 
         return redirect("explore:data_snapshots")
 

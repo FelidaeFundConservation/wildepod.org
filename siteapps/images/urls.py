feb95bef6e05b76a87329c59f1235caab9e88e2f
prabath <prabathg@gmail.com>
2023-02-16 01:37:06 -0800

    Fixing urls.py import list

--- siteapps/images/urls.py
+++ siteapps/images/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import (
-    ActivityAnnotationProcessorView,  # UploadExportView,
+    ActivityAnnotationProcessorView,
     AnnotateActivityView,
     AnnotateObjectsView,
     AnnotateSpeciesView,

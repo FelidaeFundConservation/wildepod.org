bb732210384bfc66efce0d128ec1798d60c13f5d
robson.koji <rbsnkjmr@gmail.com>
2023-03-01 18:19:54 -0300

    #31 - run pre-commit hooks

--- siteapps/explore/urls.py
+++ siteapps/explore/urls.py
@@ -4,10 +4,10 @@ from .views import (
     ExploreIndexView,
     ExploreMapView,
     ExplorePopularImagesView,
+    RemovePopularImage,
     SearchDataView,
     SnapshotCreateView,
     SnapshotListView,
-    RemovePopularImage,
 )
 
 urlpatterns = [
@@ -19,9 +19,4 @@ urlpatterns = [
     path("popular-images/remove/<uuid:pk>/", RemovePopularImage.as_view(), name="remove_popular_image"),
     path("popular-images/", ExplorePopularImagesView.as_view(), name="popular_images"),
     path("query_data/", SearchDataView.as_view(), name="query_data"),
-
-
-
-
-
 ]

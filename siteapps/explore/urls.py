1278b1af0509928e1d55cf36a0cd8fb1aa813bf4
priyanka sharmam <psharma.cad@gmail.com>
2023-03-13 10:50:03 -0700

    Modified below files to implement new feature

--- siteapps/explore/urls.py
+++ siteapps/explore/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from explore.views.set_priority import PriorityView
+
 from .views import (
     ExploreIndexView,
     ExploreMapView,
@@ -16,7 +18,12 @@ urlpatterns = [
     path("data/snapshots/", SnapshotListView.as_view(), name="data_snapshots"),
     path("map/", ExploreMapView.as_view(), name="map"),
     # path("megadetector/", ExploreMegadetectorView.as_view(), name="megadetector"),
-    path("popular-images/remove/<uuid:pk>/", RemovePopularImageView.as_view(), name="remove_popular_image"),
+    path(
+        "popular-images/remove/<uuid:pk>/",
+        RemovePopularImageView.as_view(),
+        name="remove_popular_image",
+    ),
     path("popular-images/", ExplorePopularImagesView.as_view(), name="popular_images"),
     path("query_data/", SearchDataView.as_view(), name="query_data"),
+    path("set_priority/", PriorityView.as_view(), name="set_priority"),
 ]
--- siteapps/explore/views/set_priority.py
+++ siteapps/explore/views/set_priority.py
@@ -9,40 +9,40 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, F, Q, Value
 from django.shortcuts import render
 from django.views.generic import FormView
-from images.models import Image
+from images.models import Upload
 from locations.models import CameraStation, MacroSite, MicroSite
 
 MAX_VOTES_PER_IMAGE = 2
 
 
 class SetPriorityForm(forms.Form):
-    start_date = forms.DateField(
-        widget=forms.widgets.DateInput(attrs={"type": "date"}), required=False
+    start_date = forms.SplitDateTimeField(
+        widget=forms.widgets.SplitDateTimeWidget(
+            date_attrs={"type": "date"}, time_attrs={"type": "time"}
+        ),
     )
 
-    end_date = forms.DateField(
-        widget=forms.widgets.DateInput(attrs={"type": "date"}), required=False
+    end_date = forms.SplitDateTimeField(
+        widget=forms.widgets.SplitDateTimeWidget(
+            date_attrs={"type": "date"}, time_attrs={"type": "time"}
+        ),
     )
 
     macrosites = forms.ModelMultipleChoiceField(
-        queryset=MacroSite.objects.all(), required=False
-    )
-
-    microsites = forms.ModelMultipleChoiceField(
-        queryset=MicroSite.objects.all(), required=False
+        queryset=MacroSite.objects.all(), required=True
     )
 
     camera_stations = forms.ModelMultipleChoiceField(
-        queryset=CameraStation.objects.all(), required=False
+        queryset=CameraStation.objects.all(), required=True
     )
 
     priority_choices = [
-        ("One", "Low"),
-        ("Two", "Medium"),
-        ("Three", "High"),
+        ("1", "Low"),
+        ("2", "Medium"),
+        ("3", "High"),
     ]
     priority_by = forms.ChoiceField(
-        choices=priority_choices, widget=forms.ChoiceField, initial="One"
+        choices=priority_choices, widget=forms.Select, initial="1"
     )
 
     def __init__(self, *args, **kwargs):
@@ -55,9 +55,6 @@ class SetPriorityForm(forms.Form):
             ),
             Row(
                 Column("macrosites", css_class="form-group col-12"),
-            ),
-            Row(
-                Column("microsites", css_class="form-group col-12"),
             ),
             Row(
                 Column("camera_stations", css_class="form-group col-12"),
@@ -78,7 +75,7 @@ class SetPriorityForm(forms.Form):
         self.helper.form_show_errors = True
 
 
-class SetPriorityView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
+class PriorityView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
     login_url = settings.LOGIN_URL
     template_name = "explore/set_priority.html"
     form_class = SetPriorityForm
@@ -92,70 +89,23 @@ class SetPriorityView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
             start_date = form.cleaned_data["start_date"]
             end_date = form.cleaned_data["end_date"]
             macrosites = form.cleaned_data["macrosites"]
-            microsites = form.cleaned_data["microsites"]
             camera_stations = form.cleaned_data["camera_stations"]
             priority_by = form.cleaned_data["priority_by"]
 
             # Apply the filters specified on the form on to the queryset
             filterset = {}
             if start_date:
-                filterset["trigger_timestamp__gte"] = start_date
+                filterset["date_retrieved__gte"] = start_date
             if end_date:
-                filterset["trigger_timestamp__lte"] = end_date
+                filterset["date_retrieved__lte"] = end_date
             if macrosites:
-                filterset[
-                    "upload__camera_station__micro_site__macro_site__in"
-                ] = macrosites
-            if microsites:
-                filterset["upload__camera_station__micro_site__in"] = microsites
+                filterset["camera_station__micro_site__macro_site__in"] = macrosites
             if camera_stations:
-                filterset["upload__camera_station__in"] = camera_stations
-            queryset = Image.objects.filter(**filterset)
-
-            # Group the queryset based on the breakdown_by parameter
-            # If there is no grouping use a dummy operator to aggregate all the images into a single group.
-            aggregate_column_name = ""
-            if breakdown_by == "split_none":
-                queryset = queryset.annotate(dummy_group_by=Value(" "))
-                aggregate_column_name = "dummy_group_by"
-            elif breakdown_by == "split_macrosites":
-                aggregate_column_name = (
-                    "upload__camera_station__micro_site__macro_site__name"
-                )
-            elif breakdown_by == "split_microsites":
-                aggregate_column_name = "upload__camera_station__micro_site__name"
-            elif breakdown_by == "split_camera_stations":
-                aggregate_column_name = "upload__camera_station__station_id"
-            queryset = queryset.values(aggregate_column_name).annotate(
-                name=F(aggregate_column_name)
-            )
-
-            # Finally, annotate the queryset with the counts of images in each category.
-            # This will be applied to each group specified in the values() call above.
-            queryset = queryset.annotate(
-                all_images=Count("pk", distinct=True),
-                md_processed=Count("pk", filter=Q(processed=True), distinct=True),
-                md_objects_detected=Count(
-                    "pk", filter=Q(boundingbox__gte=1), distinct=True
-                ),
-                bbox_checked=Count(
-                    "pk",
-                    filter=Q(bbox_checked_by__gte=MAX_VOTES_PER_IMAGE),
-                    distinct=True,
-                ),
-                species_checked=Count(
-                    "pk",
-                    filter=Q(species_checked_by__gte=MAX_VOTES_PER_IMAGE),
-                    distinct=True,
-                ),
-                activity_checked=Count(
-                    "pk",
-                    filter=Q(activity_checked_by__gte=MAX_VOTES_PER_IMAGE),
-                    distinct=True,
-                ),
-            ).order_by("-all_images")
+                filterset["camera_station__in"] = camera_stations
+            Upload.objects.filter(**filterset).update(priority=priority_by)
+            queryset = Upload.objects.filter(**filterset)
+            print(queryset.values())
             results = queryset
-
-            logging.info(f"Querying data : {queryset.query}")
-
-        return render(request, self.template_name, {"form": form, "results": results})
+            return render(
+                request, self.template_name, {"form": form, "results": results}
+            )

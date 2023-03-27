8a5397ee467d2b2662389e69ab932004f6ff011a
robson.koji <rbsnkjmr@gmail.com>
2023-03-27 19:10:49 -0300

    #49 - create species sighting page and related images page

--- siteapps/explore/views/__init__.py
+++ siteapps/explore/views/__init__.py
@@ -4,4 +4,4 @@ from .megadetector import ExploreMegadetectorView
 from .popular_images import ExplorePopularImagesView, RemovePopularImageView
 from .query_data import SearchDataView
 from .snapshot import SnapshotCreateView, SnapshotListView
-from .specie import SpecieSightingTimeserieView, SpecieSightingImagesView
+from .species import SpeciesSightingTimeserieView, SpeciesSightingImagesView

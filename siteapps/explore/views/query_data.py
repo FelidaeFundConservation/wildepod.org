15d08d91f1f1a63818d3eefb91cdd8036c0820ae
Prabath Gunawardane <prabath@linearsquared.com>
2023-03-29 21:17:08 -0700

    Revise the Query Data page

--- siteapps/explore/views/query_data.py
+++ siteapps/explore/views/query_data.py
@@ -2,14 +2,14 @@ import logging
 
 from braces.views import StaffuserRequiredMixin
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Button, Column, Fieldset, Layout, Row, Submit
+from crispy_forms.layout import Column, Fieldset, Layout, Row, Submit
 from django import forms
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count, Exists, F, OuterRef, Q, Subquery, Value
 from django.shortcuts import render
 from django.views.generic import FormView
-from images.models import ActivityType, Annotator, BoundingBox, Image, SpeciesName
+from images.models import Annotator, BoundingBox, Image, Species
 from locations.models import CameraStation, MacroSite
 
 MAX_VOTES_PER_IMAGE = 2
@@ -25,12 +25,22 @@ class QueryDataForm(forms.Form):
 
     camera_stations = forms.ModelMultipleChoiceField(queryset=CameraStation.objects.all(), required=False)
 
-    radio_choices = [
+    radio_choices_breakdown = [
         ("split_none", "None"),
         ("split_macrosites", "Macrosites"),
         ("split_camera_stations", "Camera Stations"),
     ]
-    breakdown_by = forms.ChoiceField(choices=radio_choices, widget=forms.RadioSelect, initial="split_none")
+    breakdown_by = forms.ChoiceField(choices=radio_choices_breakdown, widget=forms.RadioSelect, initial="split_none")
+
+    radio_choices_query = [
+        ("query_blank_ready", "Ready for Blank Pipeline"),
+        ("query_blank_completed", "Blank Pipeline Completed"),
+        ("query_species_ready", "Ready for Species Pipeline"),
+        ("query_species_completed", "Species Pipeline Completed"),
+        ("query_activity_ready", "Ready for Activity Pipeline"),
+        ("query_activity_completed", "Activity Pipeline Completed"),
+    ]
+    query_choice = forms.ChoiceField(choices=radio_choices_query, widget=forms.RadioSelect, initial="query_blank_ready")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -49,6 +59,12 @@ class QueryDataForm(forms.Form):
             Row(
                 Column(
                     Fieldset("", "breakdown_by", css_class="form-check form-check-inline"),
+                    css_class="form-group col-12",
+                )
+            ),
+            Row(
+                Column(
+                    Fieldset("", "query_choice", css_class="form-check form-check-inline"),
                     css_class="form-group col-12",
                 )
             ),
@@ -76,6 +92,7 @@ class SearchDataView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
             macrosites = form.cleaned_data["macrosites"]
             camera_stations = form.cleaned_data["camera_stations"]
             breakdown_by = form.cleaned_data["breakdown_by"]
+            query_choice = form.cleaned_data["query_choice"]
 
             # Apply the filters specified on the form on to the queryset
             filterset = {}
@@ -90,71 +107,147 @@ class SearchDataView(LoginRequiredMixin, StaffuserRequiredMixin, FormView):
 
             queryset_all = Image.objects.annotated().filter(**filterset)
 
-            # Group the queryset based on the breakdown_by parameter
-            # If there is no grouping use a dummy operator to aggregate all the images into a single group.
-            aggregate_column_name = ""
+            #TODO: Share the queries between the annotation page views and this view so we only have a single definition for these queries.
+            if query_choice == "query_blank_ready":
+                queryset_all = queryset_all.filter(
+                    # There must be at least one or more "uncertain" bounding boxes.
+                    # This will make sure that the images that need more votes are served first
+                    Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk"))),
+                    # There should not be any staff user annotations on the image already
+                    ~Exists(
+                        BoundingBox.objects.filter(
+                            Exists(Annotator.objects.filter(accepted_annotation=OuterRef("pk"), human__is_staff=True)),
+                            image=OuterRef("pk"),
+                        )
+                    ),
+                    # Image must be marked as processed by MegaDetector
+                    processed=True,
+                    # Image must have at least one bounding box
+                    num_objects__gt=0,
+                )
+            elif query_choice == "query_blank_completed":
+                queryset_all = queryset_all.filter(
+                    # There must be no "uncertain" bounding boxes, or there
+                    # should be a staff user annotation on the image.
+                    ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk")))
+                    | Exists(
+                        BoundingBox.objects.filter(
+                            Exists(Annotator.objects.filter(accepted_annotation=OuterRef("pk"), human__is_staff=True)),
+                            image=OuterRef("pk"),
+                        )
+                    ),
+                    # Image must be marked as processed by MegaDetector
+                    processed=True,
+                    # Image must have at least one bounding box
+                    num_objects__gt=0,
+                )
+            elif query_choice == "query_species_ready":
+                queryset_all = queryset_all.filter(
+                    # There must be at least one or more "valid" bounding boxes
+                    Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk"))),
+                    # There must be no uncertain bounding boxes for the image
+                    ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk"))),
+                    # TODO: Fix the line below
+                    # This is a quick and dirty hack to only ever show an image if there is at least
+                    # one bounding box that has at least one category tagged as an animal linked to it
+                    # It should work for most of the time but is not always accurate and will generate false positives
+                    # Must be fixed
+                    Exists(BoundingBox.objects.is_animal().filter(image=OuterRef("pk"))),
+                    # If a staff vote exists for the species, we'll no longer show it
+                    ~Exists(
+                        BoundingBox.objects.filter(
+                            Exists(
+                                Species.objects.filter(
+                                    Exists(
+                                        Annotator.objects.filter(
+                                            accepted_species_annotation=OuterRef("pk"), human__is_staff=True
+                                        )
+                                    ),
+                                    bounding_box=OuterRef("pk"),
+                                )
+                            ),
+                            image=OuterRef("pk"),
+                        )
+                    ),
+                    # Show image only if checked by fewer people
+                    num_species_checked_by__lt=MAX_VOTES_PER_IMAGE,
+                    # Image must be marked as processed
+                    processed=True,
+                )
+            elif query_choice == "query_species_completed":
+                queryset_all = queryset_all.filter(
+                    # There must be at least one or more "valid" bounding boxes
+                    Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk"))),
+                    # There must be no uncertain bounding boxes for the image
+                    ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk"))),
+                    # TODO: Fix the line below
+                    # This is a quick and dirty hack to only ever show an image if there is at least
+                    # one bounding box that has at least one category tagged as an animal linked to it
+                    # It should work for most of the time but is not always accurate and will generate false positives
+                    # Must be fixed
+                    Exists(BoundingBox.objects.is_animal().filter(image=OuterRef("pk"))),
+                    # Either we have a staff vote OR more than or equal to minimum consensus threshold
+                    Exists(
+                        BoundingBox.objects.filter(
+                            Exists(
+                                Species.objects.filter(
+                                    Exists(
+                                        Annotator.objects.filter(
+                                            accepted_species_annotation=OuterRef("pk"), human__is_staff=True
+                                        )
+                                    ),
+                                    bounding_box=OuterRef("pk"),
+                                )
+                            ),
+                            image=OuterRef("pk"),
+                        )
+                    ) | Q(num_species_checked_by__gte=MAX_VOTES_PER_IMAGE),
+                    # Image must be marked as processed
+                    processed=True,
+                )
+            elif query_choice == "query_activity_ready":
+                queryset_all = queryset_all.filter(
+                    # There must be at least one or more "valid" bounding boxes
+                    Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk"))),
+                    # There must be no uncertain bounding boxes for the image
+                    ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk"))),
+                    # Image must be marked as processed
+                    processed=True,
+                    num_activity_checked_by__lt=MAX_VOTES_PER_IMAGE,
+                )
+            elif query_choice == "query_activity_completed":
+                queryset_all = queryset_all.filter(
+                    # There must be at least one or more "valid" bounding boxes
+                    Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk"))),
+                    # There must be no uncertain bounding boxes for the image
+                    ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk"))),
+                    # Image must be marked as processed
+                    processed=True,
+                    num_activity_checked_by__gte=MAX_VOTES_PER_IMAGE,
+                )
+
             if breakdown_by == "split_none":
-                queryset_all = queryset_all.annotate(dummy_group_by=Value(" "))
-                aggregate_column_name = "dummy_group_by"
-            elif breakdown_by == "split_macrosites":
-                aggregate_column_name = "upload__camera_station__micro_site__macro_site__name"
-            elif breakdown_by == "split_camera_stations":
-                aggregate_column_name = "upload__camera_station__station_id"
-            queryset_all = queryset_all.values(aggregate_column_name).annotate(name=F(aggregate_column_name))
+                # If there is no grouping directly take the count() and pass it in the results.
+                results = [{
+                    'name' : ' ',
+                    'all_images' : queryset_all.count()
+                }]
+            else:
+                # Group the queryset based on the breakdown_by parameter
+                aggregate_column_name = ""
+                if breakdown_by == "split_macrosites":
+                    aggregate_column_name = "upload__camera_station__micro_site__macro_site__name"
+                elif breakdown_by == "split_camera_stations":
+                    aggregate_column_name = "upload__camera_station__station_id"
+                queryset_all = queryset_all.values('id', aggregate_column_name).values(aggregate_column_name).annotate(name=F(aggregate_column_name))
 
-            # Finally, annotate the queryset with the counts of images in each category.
-            # This will be applied to each group specified in the values() call above.
-            queryset_all = queryset_all.annotate(
-                all_images=Count("pk", distinct=True),
-                blank_ready=Count(
-                    "pk",
-                    filter=Q(processed=True)
-                    & Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk")))
-                    & Exists(BoundingBox.objects.filter(image=OuterRef("pk"))),
-                    distinct=True,
-                ),
-                blank_complete=Count(
-                    "pk",
-                    filter=Q(processed=True)
-                    & ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk")))
-                    & Exists(BoundingBox.objects.filter(image=OuterRef("pk"))),
-                    distinct=True,
-                ),
-                species_ready=Count(
-                    "pk",
-                    filter=Q(processed=True)
-                    & Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk")))
-                    & ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk")))
-                    & Exists(BoundingBox.objects.is_animal().filter(image=OuterRef("pk")))
-                    & Q(
-                        id__in=Subquery(
-                            Image.objects.filter(pk=OuterRef("pk"))
-                            .annotate(num_annotators=Count("species_checked_by"))
-                            .filter(num_annotators__lt=MAX_VOTES_PER_IMAGE)
-                            .values("id")
-                        )
-                    ),
-                    distinct=True,
-                ),
-                species_complete=Count(
-                    "pk",
-                    filter=Q(processed=True)
-                    & Exists(BoundingBox.objects.valid().filter(image=OuterRef("pk")))
-                    & ~Exists(BoundingBox.objects.uncertain().filter(image=OuterRef("pk")))
-                    & Exists(BoundingBox.objects.is_animal().filter(image=OuterRef("pk")))
-                    & Q(
-                        id__in=Subquery(
-                            Image.objects.filter(pk=OuterRef("pk"))
-                            .annotate(num_annotators=Count("species_checked_by"))
-                            .filter(num_annotators__gte=MAX_VOTES_PER_IMAGE)
-                            .values("id")
-                        )
-                    ),
-                    distinct=True,
-                ),
-            ).order_by("-all_images")
-
-            results = list(queryset_all)
+                # Finally, annotate the queryset with the counts of images in each category.
+                # This will be applied to each group specified in the values() call above.
+                queryset_all = queryset_all.annotate(
+                    all_images=Count("pk", distinct=True),
+                ).order_by("-all_images")
+    
+                results = list(queryset_all)
             logging.info(f"Querying data : {queryset_all.query}")
 
         return render(request, self.template_name, {"form": form, "results": results})

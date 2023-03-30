780b783fadaa6a81e0c03efa48ca2ab810b0ff32
devpearl <flairpearllee@gmail.com>
2023-03-29 17:28:52 -0700

    volunteer engagement track

--- siteapps/explore/urls.py
+++ siteapps/explore/urls.py
@@ -12,11 +12,13 @@ from .views import (
     SnapshotListView,
     SpeciesSightingImagesView,
     SpeciesSightingTimeseriesView,
+    VolunteerEngagementView,
 )
 
 urlpatterns = [
     path("", ExploreMapView.as_view(), name="index"),
-    path("data/snapshot/", SnapshotCreateView.as_view(), name="data_snapshot_create"),
+    path("data/snapshot/", SnapshotCreateView.as_view(),
+         name="data_snapshot_create"),
     path("data/snapshots/", SnapshotListView.as_view(), name="data_snapshots"),
     path("map/", ExploreMapView.as_view(), name="map"),
     # path("megadetector/", ExploreMegadetectorView.as_view(), name="megadetector"),
@@ -25,12 +27,16 @@ urlpatterns = [
         RemovePopularImageView.as_view(),
         name="remove_popular_image",
     ),
-    path("popular-images/", ExplorePopularImagesView.as_view(), name="popular_images"),
+    path("popular-images/", ExplorePopularImagesView.as_view(),
+         name="popular_images"),
     path("query_data/", SearchDataView.as_view(), name="query_data"),
     path("set_priority/", PriorityView.as_view(), name="set_priority"),
     path("set_priority_confirm/", ConfirmUpdateView.as_view(), name="confirm_update"),
-    re_path("species/sighting/images/$", SpeciesSightingImagesView.as_view(), name="species_sighting_images"),
+    re_path("species/sighting/images/$",
+            SpeciesSightingImagesView.as_view(), name="species_sighting_images"),
     re_path(
         "species/(?P<species>.+)/$", SpeciesSightingTimeseriesView.as_view(), name="species_sighting_timeserie_list"
     ),
+    path("volunteer_engagement/", VolunteerEngagementView.as_view(),
+         name="volunteer_engagement"),
 ]

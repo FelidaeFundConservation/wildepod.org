233b70b03ed75d922ea54bcfc2aeb0b49158571b
robson.koji <rbsnkjmr@gmail.com>
2023-03-31 18:17:46 -0300

    #79 - Blank Annotation Pipeline

--- siteapps/images/models/__init__.py
+++ siteapps/images/models/__init__.py
@@ -1,4 +1,5 @@
 from .annotation import Activity, ActivityType, BoundingBox, Category, Species, SpeciesName
 from .annotator import Annotator, Bot
 from .image import Image
+from .raw_sql import rawsql_get_blank_annotation
 from .upload import CameraStationAction, Upload

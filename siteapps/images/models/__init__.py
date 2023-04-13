f78fd3bd6bcb69a8a0873c47533b908fe0fd0518
prabath <prabathg@gmail.com>
2023-04-13 22:27:14 +0530

    Adding new bounding boxes to the Object annotation raw SQL query

--- siteapps/images/models/__init__.py
+++ siteapps/images/models/__init__.py
@@ -1,5 +1,5 @@
 from .annotation import Activity, ActivityType, BoundingBox, Category, Species, SpeciesName
 from .annotator import Annotator, Bot
 from .image import Image
-from .raw_sql import get_uncertain_images, get_images_to_ignore
+from .raw_sql import get_object_annotation_images
 from .upload import CameraStationAction, Upload

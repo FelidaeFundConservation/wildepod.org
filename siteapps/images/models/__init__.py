44172da3cdcaf3b46de5e536abc2c9a8b4abee7a
robson.koji <rbsnkjmr@gmail.com>
2023-04-18 10:50:01 -0300

    #49 - display images to annotate species

--- siteapps/images/models/__init__.py
+++ siteapps/images/models/__init__.py
@@ -1,5 +1,5 @@
 from .annotation import Activity, ActivityType, BoundingBox, Category, Species, SpeciesName
 from .annotator import Annotator, Bot
 from .image import Image
-from .raw_sql import get_uncertain_images, get_images_to_ignore, get_prioritized_images
+from .raw_sql import get_uncertain_images, get_images_to_ignore, get_prioritized_images, get_not_blank_annotation
 from .upload import CameraStationAction, Upload

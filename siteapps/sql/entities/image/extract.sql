78eab90e2831f1f1c4a9c03dd47a5478788d7bbb
robson.koji <rbsnkjmr@gmail.com>
2023-05-27 15:59:00 -0300

    #101 - add filters start end date and drop temp table for function with parameter

--- siteapps/sql/entities/image/extract.sql
+++ siteapps/sql/entities/image/extract.sql
@@ -1,14 +1,16 @@
-
-
 /* 
 * This function returns images enriched with all "static"
 * information (everything but non annotation).
 * Annotation information will be joined on image_id field.
 */
-CREATE OR REPLACE FUNCTION image_enriched(macrosite_param CHARACTER VARYING DEFAULT NULL,
-                station_id_param CHARACTER VARYING DEFAULT NULL
+CREATE OR REPLACE FUNCTION image_enriched(
+                macrosite_param CHARACTER VARYING DEFAULT NULL,
+                station_id_param CHARACTER VARYING DEFAULT NULL,
+                start_date_param TIMESTAMPTZ DEFAULT NULL,
+                end_date_param TIMESTAMPTZ DEFAULT NULL
                 ) 
-RETURNS TABLE (image_id UUID,
+RETURNS TABLE (
+                image_id UUID,
                 dropbox_content_hash CHARACTER VARYING, 
                 dropbox_file_name TEXT,
                 thumbnail_gcloud_path CHARACTER VARYING, 
@@ -25,12 +27,16 @@ RETURNS TABLE (image_id UUID,
                 dropbox_folder_path TEXT,
                 social_media_worthy INTEGER,
                 bbox_checked_by_count BIGINT,
-                -- category CHARACTER VARYING
                 species_checked_by_count BIGINT
-                -- species CHARACTER VARYING
                 ) AS 
 $$
 BEGIN
+    /* 
+    To check rules to drop or use cache.
+    This is to avoid caching on different function calls
+    with different arguments.
+    */
+    DROP TABLE IF EXISTS temp_image_enriched;
     CREATE TEMPORARY TABLE temp_image_enriched AS (
         SELECT  
             image.id AS image_id,
@@ -51,24 +57,10 @@ BEGIN
             image.social_media_worthy, 
             (SELECT COUNT(1) 
                 FROM images_image_bbox_checked_by AS bbox_checked_by
-                WHERE bbox_checked_by.image_id = image.id) AS bbox_checked_by_count,
-            -- category.name AS category
+                WHERE bbox_checked_by.image_id = image.id) AS bbox_checked_by_count,            
             (SELECT COUNT(1) 
                 FROM images_image_species_checked_by AS species_checked_by
-                WHERE species_checked_by.image_id = image.id) AS species_checked_by_count
-            --     /*            
-            --     "detected_objects",
-            --     "validated_objects",
-            --     "uncertain_objects",
-            --     "objects",
-
-            --     "validated_species",
-            --     "uncertain_species",
-            --     "species",
-            --     */
-            
-            -- COALESCE(species_name.name, '') AS species
-            
+                WHERE species_checked_by.image_id = image.id) AS species_checked_by_count            
 
         FROM images_image AS image 
         LEFT JOIN images_upload AS upload
@@ -81,18 +73,11 @@ BEGIN
             ON microsite.macro_site_id = macrosite.id
         LEFT JOIN users_user AS userr
             ON userr.id = upload.volunteer_id    
-        -- LEFT JOIN images_boundingbox AS bbox
-        --     ON image.id = bbox.image_id
-        -- LEFT JOIN images_category AS category
-        --     ON bbox.id = category.bounding_box_id
-        -- LEFT JOIN images_species AS species
-        --     ON bbox.id = species.bounding_box_id
-        -- LEFT JOIN images_speciesname AS species_name
-        --     ON species.name_id = species_name.id
 
-        WHERE macrosite_param IS NULL OR macrosite.name = macrosite_param
-            AND station_id_param IS NULL OR camera_station.station_id = station_id_param
-        -- LIMIT 100
+        WHERE (macrosite_param IS NULL OR macrosite.name = macrosite_param)
+          AND (station_id_param IS NULL OR camera_station.station_id = station_id_param)
+          AND (start_date_param IS NULL OR image.trigger_timestamp >= start_date_param)
+          AND (end_date_param IS NULL OR image.trigger_timestamp <= end_date_param)       
     );
 
     RETURN QUERY 
@@ -102,8 +87,9 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-select * from image_enriched()
-where image_id = '000009e8-4835-4819-a75f-bce609d85d51'
+-- SELECT * from image_enriched(start_date_param := '2020-01-01')
+-- WHERE image_id = '000009e8-4835-4819-a75f-bce609d85d51'
+-- AND trigger_timestamp >= '2021-01-01'
 
 
 -- SELECT * from images_image

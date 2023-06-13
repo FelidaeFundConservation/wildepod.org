d8ceb52378d883796dda7f988a64ae0b7931fe12
robson.koji <rbsnkjmr@gmail.com>
2023-06-13 19:40:13 -0300

    #116 - first working code, with a simulator

--- siteapps/vote_results/models.py
+++ siteapps/vote_results/models.py
@@ -1,19 +1,24 @@
-from django.conf import settings
-from django.db import models
-from model_utils.models import TimeStampedModel
-from simple_history.models import HistoricalRecords
-# Create your models here.
+import uuid
 
+from django.db import models
+from django.db.models import Q
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+
+from model_utils.models import TimeStampedModel
+from simple_history.models import HistoricalRecords
+
+from images.models import BoundingBox, Category, Species, Activity
 
 
 class VoteResults(TimeStampedModel):
     CLOSE_CHOICES= (('SE', 'Staff Expert Rule'), ('DV', 'Difference of Votes'))
     
     # Generic Foreign Key to Bounding Box, Category, Species, Activity
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)    
+    object_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
     content_object = GenericForeignKey()
     
     # Null = UNCERTAIN  -   voting running
@@ -27,43 +32,44 @@ class VoteResults(TimeStampedModel):
         # check rule of difference to close an election or keep open.
         # abs(valid - invalid) + new_vote 
         votes = self.get_votes()        
-        total_votes = votes.filter(vote=1).count() - votes.filter(vote=0).count()
+        total_votes = 0         
+        if votes:
+            total_votes = votes.filter(vote=1).count() - votes.filter(vote=0).count()
+                    
         total_votes += 1 if new_vote else -1            
         if total_votes >= 2: 
             self.vote_state = True
-        elif total_votes <= 2: 
+        elif total_votes <= -2: 
             self.vote_state = False
         else:            
             return None # keep voting open
         return True # close voting    
     
     def has_staff_expert_vote(self):
-        self.get_votes()
-        if self.votes.filter(annotator__human__is_staff=True).exists() or \
-                    self.votes.filter(annotator__human__is_expert=True).exists():
+        votes = self.get_votes()
+        if votes.filter(annotator__human__is_staff=True).exists() or \
+                    votes.filter(annotator__human__is_expert=True).exists():
                         return True
         return False
         
     def is_open(self):                        
         if self.vote_state is not None or self.close_rule is not None:
-            raise Exception("VoteResults is not open for voting for this object: {}".format(self.diputed_object))
+            raise Exception("Election is closed, no more votes are accepted:\n{}".format(self))
         return True
 
     def get_votes(self):
-        self.votes = self.registrevote_set.all()
+        return self.registervote_set.all()
         
     def __str__(self):
-        return "content_type: {}, object_id: {}, vote_state: {}, close_rule: {}, history: {}".format(
-                    self.content_type, 
-                    self.object_id, 
-                    self.vote_state, 
-                    self.close_rule, 
-                    str(self.history)
-                )
+        return "{} id: {} \nVote State: {}\nClose Rule: {}".format(self.content_type.model.upper(), 
+                                                                    self.object_id, 
+                                                                    self.vote_state, 
+                                                                    self.close_rule
+                                                                    )
                 
     class Meta:
-        # Constrain for close_rule only can be not null when vote_state is not null 
         constraints = [
+            # Constrain for close_rule only can be not null when vote_state is not null 
             models.CheckConstraint(
                 check=Q(vote_state__isnull=True) | Q(close_rule__isnull=False),
                 name='close_rule_null_when_vote_state_null'
@@ -72,14 +78,16 @@ class VoteResults(TimeStampedModel):
     
     
 class RegisterVote(TimeStampedModel):       
+    from images.models import Annotator
+
     # 0 = INVALID - diputed_object is invalid
     # 1 = VALID   - diputed_object is valid
     vote = models.BooleanField()    
-    annotator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, blank=True, null=True)
+    annotator = models.ForeignKey(Annotator, on_delete=models.PROTECT, blank=True, null=True)
     comment = models.TextField()
     vote_results = models.ForeignKey(VoteResults, on_delete=models.CASCADE)    
         
-    def annotator_vote(self, annotator, diputed_object, vote, comment=''):        
+    def pick_vote(self, annotator, diputed_object, vote, comment=''):   
         try:
             self._get_object_class(diputed_object)
         except:
@@ -88,8 +96,7 @@ class RegisterVote(TimeStampedModel):
         self.vote = vote
         self.annotator = annotator
         self.comment = comment        
-        obj, created = VoteResults.get_or_create(content_object=self.content_object, vote_state=vote)
-        self.vote_results = obj
+        self.vote_results, created = VoteResults.objects.get_or_create(content_type=self.content_type, object_id=self.object_id)
         
         """
         1. If vote from staff or expert, register vote and close the VoteResults.
@@ -99,7 +106,11 @@ class RegisterVote(TimeStampedModel):
            based on the rule of difference.
         """
         if self._is_staff_expert(annotator):
-            self.vote_results.vote_state = 1 if vote else 0
+            # Double check. This should not be necessary.
+            # If staff_expert can chage a closed election, remove this. 
+            if not self.vote_results.is_open():
+                raise                    
+            self.vote_results.vote_state = True if self.vote else False
             self.vote_results.close_rule = 'SE'
         elif not created:
             # Double check. This should not be necessary.
@@ -114,17 +125,26 @@ class RegisterVote(TimeStampedModel):
         self.vote_results.save()
         self.save()
     
-    def _is_staff_expert(annotator):
-        # expects an annotator object
-        return annotator.objects.get(Q(human__is_staff=True) | Q(human__is_expert=True)) 
+    def _is_staff_expert(self, annotator):
+        return annotator.human.is_staff or annotator.human.is_expert
 
     def _get_object_class(self, diputed_object):                
-        # Import here to avoid circular import
-        from annotations.models import BoundingBox, Category, Species, Activity    
-
         classes = [BoundingBox, Category, Species, Activity]
         for klass in classes:
             if isinstance(diputed_object, klass):                
-                self.content_object = klass.objects.get(id=diputed_object.id)
-        raise Exception("Disputed Object not found")
+                try:
+                    self.content_type = content_type = ContentType.objects.get_for_model(klass)
+                    self.object_id = diputed_object.id
+                except klass.DoesNotExist:           
+                    raise Exception("Disputed Object not found")
         
+    def __str__(self):
+        return "Annotator: {}\nVote: {}\nDate: {}".format(self.annotator, self.vote, self.created)
+
+
+    class Meta:
+        constraints = [
+            # One annotator cannot vote twice for the same disputed object
+            models.UniqueConstraint(fields=['annotator', 'vote_results'], name='unique_annotator_vote_results_constraint')
+        ]        
+            
--- siteapps/vote_results/tests.py
+++ siteapps/vote_results/tests.py
@@ -1,3 +1,112 @@
+"""
+This script makes a simulation of voting. 
+It is useful to test the rules of voting, in the models
+Cleanup your tables when needed: 
+    vote_results_historicalvoteresults
+    vote_results_registervote
+    vote_results_voteresults
+"""
+
+import random
+
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-# Create your tests here.
+from images.models import BoundingBox, Category, Species, Activity, Annotator
+from .models import VoteResults, RegisterVote
+
+
+
+# Get random 10 objects for each model
+categories = Category.objects.all().order_by('?')[:10]
+species = Species.objects.all().order_by('?')[:10]
+bouding_boxes = BoundingBox.objects.all().order_by('?')[:10]
+activities = Activity.objects.all().order_by('?')[:10]
+objects = [categories, species, bouding_boxes, activities]
+
+# A list of random annotators. The first one is a staff user, 
+# so a staff/expert rule should apply in this case. 
+# For the other users, the rule of difference should apply.
+annotators = ["b638c541-f341-4328-b30a-81392f25b334", 
+                "e5a1561b-7eb9-40e8-b54f-1ac14506ac26",
+                "32474ca4-5a95-4a05-ae2a-2312aa6736fd",
+                "722746e7-6b16-4e30-9818-4b681dd54398",
+                "821d9237-cb3d-48f6-8f36-975fda991182",
+                "fd7e2627-63d3-43dd-9e39-ecc52bdc1dee",
+                "40b1be5e-f326-40ed-9217-703d31da78f6"]
+
+
+def get_random_element(lst):
+    return random.sample(lst, 1)
+
+def check_vote_result(content_type, obj, before=False):
+    try:
+        vr = VoteResults.objects.get(content_type=content_type, object_id=obj.id)                
+        if before: print('Election exists:') 
+        print(vr)    
+        return vr
+    except VoteResults.DoesNotExist:
+        print('Election does not exist. It will be created for:')
+        print('{}: {}'.format(content_type.model.upper(), obj.id))
+        
+def print_votes(votes):
+    for vote in votes:
+        print(vote)
+
+
+def vote():
+    # Vote setup
+    human_id = get_random_element(annotators)[0]
+    annotator = Annotator.objects.get(human_id=human_id)
+    obj = get_random_element(objects)[0][0]
+    vote = get_random_element([True, False])[0]
+    content_type = ContentType.objects.get_for_model(type(obj))       
+
+    print('\n\n\n\n==================================================')    
+    print('ELECTION STATUS BEFORE NEW VOTE')    
+    print('-------------------------------')
+    vr = check_vote_result(content_type, obj, True)
+        
+
+    # Vote
+    try:
+        rv = RegisterVote()    
+        rv.pick_vote(annotator=annotator, diputed_object=obj, vote=vote, comment='')
+        print('\n\nNEW VOTE')    
+        print('--------')        
+        print(rv)
+    except Exception as e:
+        print('\n\nEXCEPTION ON VOTE')
+        print('-----------------')        
+        print(e)
+    
+        print('\nVOTE(S)')    
+        print('-------')    
+        votes = vr.get_votes()        
+        print_votes(votes)        
+        print('==================================================')   
+        return
+    
+    # Print Vote Result
+    print('\n\nELECTION STATUS AFTER NEW VOTE')    
+    print('------------------------------')
+    vr = check_vote_result(content_type, obj)
+    votes = vr.get_votes()
+    
+    print('\nVOTE(S)')    
+    print('-------')    
+    print_votes(votes)
+    
+    print 
+    print('==================================================')
+    
+
+idx = 0     
+while idx < 100:
+    vote()
+    idx += 1    
+
+"""
+TO RUN TEST:
+./manage.py test vote_results --settings=config.settings.local_settings
+"""

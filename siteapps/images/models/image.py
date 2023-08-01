b1603ce36b270adb5748438447ffceb77e2196bf
Justin B. Yee <jbyee2015@gmail.com>
2023-07-31 17:24:19 -0700

    Added fields for precomputed pipeline stage-related flags to Image model, and implemented calculations for flagging existence of humans, animals, and vehicles in category annotation submission.

--- siteapps/images/models/image.py
+++ siteapps/images/models/image.py
@@ -108,6 +108,16 @@ class Image(TimeStampedModel):
     # Flag for Staff Review. This field is used to indicate images that should be reviewed later by staff users.
     staff_review_needed = models.BooleanField(default=False)
 
+    # Precomputed pipeline stage-related flags.
+    category_pipeline_complete = models.BooleanField(default=False)
+    species_pipeline_complete = models.BooleanField(default=False)
+    activity_pipeline_complete = models.BooleanField(default=False)
+
+    has_animals = models.BooleanField(default=False)
+    has_humans = models.BooleanField(default=False)
+    has_vehicles = models.BooleanField(default=False)
+    has_wild_animals = models.BooleanField(default=False)
+
     # Custom manager
     objects = ImageManager()
 

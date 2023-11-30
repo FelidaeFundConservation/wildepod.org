319c2f52104ba205ae473cafb6f3bf3c9e50c0aa
Justin B. Yee <jbyee2015@gmail.com>
2023-11-30 11:41:55 -0800

    Parameter tweaking.

--- siteapps/images/management/commands/export/train.py
+++ siteapps/images/management/commands/export/train.py
@@ -1,16 +1,20 @@
+import torch
 from ultralytics import YOLO
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
+
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n.pt").to(device)
 
 # Train the model using the 'coco128.yaml' dataset for 3 epochs
-results = model.train(data="config.yaml", epochs=3)
+results = model.train(data="config.yaml", epochs=10)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
 
 # Perform object detection on an image using the model
-results = model("./datasets/wildepod/images/val/0c333207-9c1b-45c0-9ebc-bd1bd7cc1e7b.jpg")
+# results = model("./datasets/wildepod/images/val/0c333207-9c1b-45c0-9ebc-bd1bd7cc1e7b.jpg")
 
 # Export the model to ONNX format
 success = model.export(format="onnx")

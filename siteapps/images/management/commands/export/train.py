b526edc50a19961c9202d1e1fed118d2ccc53daf
Justin B. Yee <jbyee2015@gmail.com>
2023-12-03 11:11:08 -0800

    Using GPU to train.

--- siteapps/images/management/commands/export/train.py
+++ siteapps/images/management/commands/export/train.py
@@ -2,19 +2,21 @@ import torch
 from ultralytics import YOLO
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}")
 
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO("yolov8n.pt").to(device)
+model = YOLO("./runs/detect/train/weights/last.pt").to(device)
 
-# Train the model using the 'coco128.yaml' dataset for 3 epochs
-results = model.train(data="config.yaml", epochs=10)
+if __name__ == "__main__":
+    print(f"Using device: {device}")
 
-# Evaluate the model's performance on the validation set
-results = model.val()
+    # Train the model using the 'coco128.yaml' dataset for x epochs
+    # results = model.train(data="config.yaml", epochs=600, batch=-1, workers=1, exist_ok=True, resume=True)
+    results = model.train(resume=True)
+    # Evaluate the model's performance on the validation set
+    results = model.val()
 
-# Perform object detection on an image using the model
-# results = model("./datasets/wildepod/images/val/0c333207-9c1b-45c0-9ebc-bd1bd7cc1e7b.jpg")
+    # Perform object detection on an image using the model
+    # results = model("./datasets/wildepod/images/val/0c333207-9c1b-45c0-9ebc-bd1bd7cc1e7b.jpg")
 
-# Export the model to ONNX format
-success = model.export(format="onnx")
+    # Export the model to ONNX format
+    success = model.export(format="onnx")
--- siteapps/images/management/commands/export_yolo_model_data.py
+++ siteapps/images/management/commands/export_yolo_model_data.py
@@ -1,4 +1,5 @@
 import logging
+import random
 from io import BytesIO
 
 import requests
@@ -19,14 +20,61 @@ class Command(BaseCommand):
 
             logging.info(f"Gathering data from {IMAGE_COUNT} images...")
 
-            images = Image.objects.filter(has_wild_animals=True)[:IMAGE_COUNT]
+            speciesname_list = SpeciesName.objects.all()
 
+            images = []
+
+            for name in speciesname_list:
+                name_results = list(
+                    Image.objects.filter(
+                        Q(has_wild_animals=True)
+                        | Q(species_checked_by__human__is_staff=True)
+                        | Q(species_checked_by__human__is_expert=True),
+                        boundingbox__species__name__id=name.id,
+                    ).distinct()[:IMAGE_COUNT]
+                )
+                images += name_results
+                print(f"Got {len(name_results)} images for class {name.name}.")
+
+            with open(f"{PATH}config.yaml", "w+") as file:
+                classes = list(SpeciesName.objects.all())
+
+                classes_list = []
+                classes_dict = {}
+
+                classIndex = 0
+
+                for species in classes:
+                    classes_list.append(f"  {classIndex}: '{species.name}'\n")
+                    classes_dict[f"{species.name}"] = classIndex
+                    classIndex += 1
+
+                lines = [
+                    "path: ./\n",
+                    "train: ./wildepod/images/train/\n",
+                    "test: ./wildepod/images/test/\n",
+                    "val: ./wildepod/images/val/\n",
+                    "\n",
+                    f"nc: {len(classes_list)}\n",
+                    "\n",
+                    f"names: \n",
+                ]
+
+                lines += classes_list
+
+                file.writelines(lines)
+
+            input("Do you want to retrieve this data? [ENTER]")
+
+            random.shuffle(images)
             split_index = int(len(images) * 0.7)
 
             training = images[:split_index]
             validation = images[split_index:]
 
             def get_data(images, directory):
+                length = len(images)
+
                 for image in images:
                     image_file_path = f"{settings.MEDIA_URL}{image.thumbnail_gcloud_path}"
                     response = requests.get(image_file_path)
@@ -38,10 +86,6 @@ class Command(BaseCommand):
                             for bbox in image.boundingbox_set.valid().all():
                                 species = (
                                     Species.objects.filter(
-                                        Q(created_by__human__is_staff=True)
-                                        | Q(created_by__human__is_expert=True)
-                                        | Q(accepted_by__human__is_staff=True)
-                                        | Q(accepted_by__human__is_expert=True),
                                         bounding_box=bbox,
                                     )
                                     .distinct()
@@ -49,31 +93,13 @@ class Command(BaseCommand):
                                 )
 
                                 if species:
-                                    info = f"{species.name.id} {bbox.x + (bbox.w / 2)} {bbox.y + (bbox.h / 2)} {bbox.w} {bbox.h}\n"
-                                    print(info)
+                                    info = f"{classes_dict.get(f'{species.name.name}')} {bbox.x + (bbox.w / 2)} {bbox.y + (bbox.h / 2)} {bbox.w} {bbox.h}\n"
+                                    print(f"{length} to go: {info}")
                                     file.write(info)
+                    length -= 1
 
             get_data(training, "train")
             get_data(validation, "val")
-
-            with open(f"{PATH}config.yaml", "w+") as file:
-                classes = list(SpeciesName.objects.all().values("id"))
-
-                classes_list = []
-                for species in classes:
-                    classes_list.append(str(species["id"]))
-
-                lines = [
-                    "path: ./\n",
-                    "train: ./wildepod/images/train/\n",
-                    "test: ./wildepod/images/test/\n",
-                    "val: ./wildepod/images/val/\n",
-                    "\n",
-                    f"nc: {len(classes_list)}\n",
-                    "\n",
-                    f"names: {str(classes_list)}\n",
-                ]
-                file.writelines(lines)
 
         except Exception as e:
             raise CommandError(e)

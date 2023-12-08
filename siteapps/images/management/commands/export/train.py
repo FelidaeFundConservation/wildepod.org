8c23e0e494032c1dab34f960567ff6ddb452fb91
Justin B. Yee <jbyee2015@gmail.com>
2023-12-08 10:35:34 -0800

    Configuration.

--- siteapps/images/management/commands/export/train.py
+++ siteapps/images/management/commands/export/train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 resume_pt = "./runs/detect/train/weights/last.pt"
-pt = "wildepod-species-12-03-2023.pt"
+pt = "yolov8n.pt"
 
 # Load a pretrained YOLO model (recommended for training)
 model = YOLO(pt).to(device)
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     print(f"Using device: {device}")
 
     # Train the model using the 'coco128.yaml' dataset for x epochs
-    results = model.train(data="config.yaml", epochs=1500, batch=32, workers=1, exist_ok=True)
+    results = model.train(data="config.yaml", epochs=300, batch=-1, workers=4, exist_ok=True, pretrained="best.pt")
     # results = model.train(resume=True)
 
     # Evaluate the model's performance on the validation set

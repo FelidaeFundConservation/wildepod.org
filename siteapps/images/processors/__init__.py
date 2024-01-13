3d684a5678e33353d0e72709609f24d64b71897f
Justin B. Yee <jbyee2015@gmail.com>
2024-01-13 14:23:17 -0800

    Added AI detection of species, including 2nd gen cloud function on GCP.

--- siteapps/images/processors/__init__.py
+++ siteapps/images/processors/__init__.py
@@ -1,3 +1,3 @@
 from .annotation import process_activity_annotations, process_md_annotations, process_species_annotations, vote
-from .image import process_image
+from .image import process_image, run_model_inference
 from .upload import process_upload

fd12f38455091b8bfc4a9eaa7ccf949db531179c
Justin B. Yee <jbyee2015@gmail.com>
2024-01-18 13:14:44 -0800

    Run inference during uploading processing, and made image page consistent with annotation pages.

--- siteapps/images/processors/upload.py
+++ siteapps/images/processors/upload.py
@@ -8,7 +8,7 @@ import dropbox
 from django.conf import settings
 from images.models import Image, Upload
 
-from .image import process_image
+from .image import process_image, run_model_inference
 
 # Create a dropbox client
 dbx = dropbox.Dropbox(

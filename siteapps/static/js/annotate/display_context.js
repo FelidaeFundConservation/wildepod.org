2da37ec06d77c08c9bf1b454a457a6f0d12701d2
Justin B. Yee <jbyee2015@gmail.com>
2024-08-14 16:57:39 -0700

    Broke out multiple other features in the annotation page to js files.

---
+++ siteapps/static/js/annotate/display_context.js
@@ -0,0 +1,40 @@
+contextImageTimeout = null;
+
+const vars = $('meta[name="django-vars"]');
+imageId = `main-${vars.attr('image-id')}`;
+imageSrc = vars.attr('main-img-src');
+
+// Show the context image in place of the original
+function displayContextImage(contextImage) {
+    var mainImage = document.getElementById(imageId);
+    mainImage.src = contextImage.src;
+    $(".a9s-annotationlayer").hide();
+
+    if (contextImageTimeout) {
+        clearTimeout(contextImageTimeout);
+    }
+}
+
+
+// Change the displayed image back to the original image being annotated
+function revertContextImage() {
+    var mainImage = document.getElementById(imageId);
+
+    // Delay switching images to avoid flickering
+    contextImageTimeout = setTimeout(function () {
+        let anyImgHovered = false;
+
+        $('#context-image-container').find('img').each(function() {
+            if ($(this).is(':hover')) {
+                anyImgHovered = true;
+                return false;
+            }
+        });
+
+        // Don't switch image if still hovering context
+        if (!anyImgHovered) {
+            mainImage.src = imageSrc;
+            $(".a9s-annotationlayer").show();
+        }
+    }, 1000)
+}

7bae60a4c0b4c52c157affed34f1968ff82ba8f8
Justin B. Yee <jbyee2015@gmail.com>
2024-10-18 11:07:41 -0700

    Setup url routing for ind. volunteer stats pages.

--- siteapps/users/urls.py
+++ siteapps/users/urls.py
@@ -6,11 +6,13 @@ from .views import (
     VolunteerListView,
     VolunteerRegisterSuccessView,
     VolunteerRegisterView,
+    VolunteerStatsView,
 )
 
 urlpatterns = [
     path("profile/", view=UserUpdateView.as_view(), name="profile"),
     path("volunteers/", view=VolunteerListView.as_view(), name="volunteers_list"),
+    path("volunteers/stats/<uuid:pk>/", view=VolunteerStatsView.as_view(), name="volunteer_stats"),
     path("volunteers/add/", view=VolunteerRegisterView.as_view(), name="volunteer_add"),
     path("volunteers/add/success", view=VolunteerRegisterSuccessView.as_view(), name="volunteer_added"),
     path("prioritize_animals/", view=PrioritizeTaggingAnimalsView.as_view(), name="prioritize_animals"),

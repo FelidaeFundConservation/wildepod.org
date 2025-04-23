8311b8397c3fee311bb8b170bf63d3d0ee8d659b
prabath <prabathg@gmail.com>
2025-04-23 15:10:53 -0500

    Refactoring to move Google Maps API key to an environment variable

--- siteapps/home/context_processors.py
+++ siteapps/home/context_processors.py
@@ -7,4 +7,5 @@ def global_settings(request):
         'is_prod': 'prod' in settings.WSGI_APPLICATION,
         'is_staging': 'staging' in settings.WSGI_APPLICATION,
         'is_bhutan': 'bhutan' in settings.WSGI_APPLICATION,
+        'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     }

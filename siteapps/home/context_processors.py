f751d7a2f46782d5e40d68300f2381e1e3e2adde
Prabath Gunawardane <prabathg@gmail.com>
2025-12-23 23:25:06 -0800

    Cleanup code for Local server startup. Removed unncessary GCloud initializations when runnning strictly locally

--- siteapps/home/context_processors.py
+++ siteapps/home/context_processors.py
@@ -7,5 +7,6 @@ def global_settings(request):
         'is_prod': 'prod' in settings.WSGI_APPLICATION,
         'is_staging': 'staging' in settings.WSGI_APPLICATION,
         'is_bhutan': 'bhutan' in settings.WSGI_APPLICATION,
+        'is_local': 'local' in settings.WSGI_APPLICATION,
         'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
     }
